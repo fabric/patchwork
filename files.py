d03e32e665482d931429925b6445cc8dbaa096d4
Jeff Forcier <jeff@bitprophet.org>
2012-04-26 18:26:00 -0700

    Start basic file stuff

---
+++ files.py
@@ -0,0 +1,22 @@
+from fabric.api import sudo
+
+
+def directory(d, user=None, group=None, mode=None):
+    """
+    Ensure a directory exists and has given user and/or mode
+    """
+    sudo("mkdir -p %s" % d)
+    if user is not None:
+        group = group or user
+        sudo("chown %s:%s %s" % (user, group, d))
+    if mode is not None:
+        sudo("chmod %s %s" % (mode, d))
+
+
+def exists(path):
+    """
+    Return True if given path exists on the current remote host.
+    """
+    cmd = 'test -e "$(echo %s)"' % path
+    with settings(warn_only=True):
+        return sudo(cmd).succeeded
