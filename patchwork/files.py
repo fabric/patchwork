bc3ecdade8534ca6159dc3534253d23427c7af1d
Jeff Forcier <jeff@bitprophet.org>
2012-05-04 10:16:22 -0700

    Switch patchwork.files to allow runner override

--- patchwork/files.py
+++ patchwork/files.py
@@ -1,22 +1,22 @@
-from fabric.api import sudo, settings, hide
+from fabric.api import run, settings, hide
 
 
-def directory(d, user=None, group=None, mode=None):
+def directory(d, user=None, group=None, mode=None, runner=run):
     """
     Ensure a directory exists and has given user and/or mode
     """
-    sudo("mkdir -p %s" % d)
+    runner("mkdir -p %s" % d)
     if user is not None:
         group = group or user
-        sudo("chown %s:%s %s" % (user, group, d))
+        runner("chown %s:%s %s" % (user, group, d))
     if mode is not None:
-        sudo("chmod %s %s" % (mode, d))
+        runner("chmod %s %s" % (mode, d))
 
 
-def exists(path):
+def exists(path, runner=run):
     """
     Return True if given path exists on the current remote host.
     """
     cmd = 'test -e "$(echo %s)"' % path
     with settings(hide('everything'), warn_only=True):
-        return sudo(cmd).succeeded
+        return runner(cmd).succeeded
