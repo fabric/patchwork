ca70ed2d431a62ea33343279476aa37bcd1bd6c8
Jeff Forcier <jeff@bitprophet.org>
2018-05-05 12:51:19 -0700

    Set up for testing

--- tasks.py
+++ tasks.py
@@ -1,11 +1,11 @@
-from invocations import docs
+from invocations import docs, travis
 from invocations.packaging import release
-from invocations import travis
+from invocations.pytest import test
 
 from invoke import Collection
 
 
-ns = Collection(docs, release, travis)
+ns = Collection(docs, release, travis, test)
 ns.configure({
     'packaging': {
         'sign': True,
