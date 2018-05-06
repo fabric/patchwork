c8e2230a77dcf9502770862a4327d35cbb612287
Jeff Forcier <jeff@bitprophet.org>
2018-05-05 18:07:05 -0700

    Start in on Travis, why not

--- tasks.py
+++ tasks.py
@@ -1,11 +1,26 @@
+from __future__ import unicode_literals, print_function
+
+from importlib import import_module
+
 from invocations import docs, travis
 from invocations.packaging import release
 from invocations.pytest import test
 
-from invoke import Collection
+from invoke import Collection, task
 
 
-ns = Collection(docs, release, travis, test)
+@task
+def sanity(c):
+    """
+    Quick sanity check to ensure we're installed successfully. Mostly for CI.
+    """
+    # Doesn't need to literally import everything, but "a handful" will do.
+    for name in ('environment', 'files', 'transfers'):
+        import_module("patchwork.{}".format(name))
+    print("\U0001F44D") # FUN WITH UNICODE
+
+
+ns = Collection(docs, release, travis, test, sanity)
 ns.configure({
     'packaging': {
         'sign': True,
